80e78d7037fb1a688a5d53abfd09d878e7111c3e
Askar Pulatov <master15994@gmail.com>
2023-03-23 11:21:01 +0300

    finish part 6

--- Массивы.js
+++ Массивы.js
@@ -1,22 +1,64 @@
-let str = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//------------------------------ Замыкание -------------------------
+let createCounter = function (counterName) {
+  let counter = 0;
 
-// let new = str.concat([1,2])
+  return {
+    increment: function () {
+      counter++;
+    },
+    decrement: function () {
+      counter--;
+    },
+    getCounter: function () {
+      return counter;
+    },
+  };
+};
 
-let obj = [
-  { name: 'Tan', age: 63 },
-  { name: 'Lola', age: 233 },
-  { name: 'Kiss', age: 32 },
-];
+let counterA = createCounter('counter A');
+let counterB = createCounter('counter B');
 
-let person = obj.find(function (per) {
-  return per.age === 63;
-});
-console.log(person);
+//---------------------------Доступ ко всем ключам -------------------------
 
-let odd = [1, 2, 3, 4, 5, 6, 7, 8].filter(function (i) {
-  return i % 2 !== 0;
-});
-console.log(odd);
+let person = {
+  name: 'Max',
+  age: 23,
+  job: 'IT',
+};
+
+// ---------------Первый способ ------------------
+// for (let key in person) {
+//   console.log(person[key]);
+// }
+
+//-----------------Второй способ -----------------
+
+// let keys = Object.keys(person);
+// console.log(keys);
+
+// Object.keys(person).forEach(function (key) {
+//   console.log(person[key]);
+// });
+
+// let str = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//   let new = str.concat([1,2])
+
+// let obj = [
+//   { name: 'Tan', age: 63 },
+//   { name: 'Lola', age: 233 },
+//   { name: 'Kiss', age: 32 },
+// ];
+
+// let person = obj.find(function (per) {
+//   return per.age === 63;
+// });
+// console.log(person);
+
+// let odd = [1, 2, 3, 4, 5, 6, 7, 8].filter(function (i) {
+//   return i % 2 !== 0;
+// });
+// console.log(odd);
 
 // let cars = ['BMW', 'Mersedez', 'Tesla', 'Hyaka'];
 
--- Обьекты и Массивы/script.js
+++ Обьекты и Массивы/script.js
@@ -1,67 +1,184 @@
-let ford = Object.create(
-  // Object - глобальный обьект; create - метод
+let a = [1, 2, 4];
+let b = [5, 'Hello', 6];
 
-  // Первый Параметр - create
-  {
-    calculateDistancePerYear: function () {
-      // высчитываем сколько машина проезжает за год
-      Object.defineProperty(this, 'distancePerYear', {
-        //1. this - обьект , 2. название , 3. создаем обьект
-        value: Math.ceil(this.distance / this.age), // math.ceil - округляет число до целого
-        enumerable: true,
-        writable: false,
-        configurable: false,
-      });
-    },
-  },
+Array.prototype.double = function () {
+  let newArray = this.map(function (item) {
+    if (typeof item === 'number') {
+      return (item *= item);
+    }
+  });
 
-  //Второй Параметр - create
-  {
-    name: {
-      value: 'Ford',
-      enumerable: true, // позволяет открыть область видимости для опред. свойств и будет дост. для перечесление
-      writable: false, // св-во может быть изменино если значение true (по умол. знач false), нельзя поменять 'Ford'
-      configurable: false, // мы можем удалять или изменять ключи из обьекта если знач true (по умол. знач false)
-    },
-    model: {
-      value: 'Focus',
-      enumerable: true,
-      writable: false,
-      configurable: false,
-    },
-    year: {
-      value: 1997,
-      enumerable: true,
-      writable: false,
-      configurable: false,
-    },
-    distance: {
-      value: 144888,
-      enumerable: true,
-      writable: true,
-      configurable: false,
-    },
-
-    age: {
-      // высчитываем возраст машины
-      enumerable: true,
-      get: function () {
-        console.log('Получаем возраст');
-        return new Date().getFullYear() - this.year; //создаем новый обект(new Date)-вызываем метод(текущий год ) - значение
-      },
-      set: function () {
-        console.log('Устанавливаем значение ');
-      },
-    },
+  if (typeof item == 'string') {
+    return (item += item);
   }
-);
+  return newArray;
+};
 
-console.log(ford);
+let newA = a.double();
+let newB = b.double();
 
-for (let key in ford) {
-  // получаем ключи нашего обьекта
-  console.log(key);
-}
+console.log('A', newA);
+console.log('B', newB);
+
+// * Привязка Контекст
+
+// function printObject(objName) {
+//   console.log('Printing Object', objName);
+//   for (let key in this) {
+//     if (this.hasOwnProperty(key)) {
+//       console.log('[' + key + ']', this[key]);
+//     }
+//   }
+// }
+
+// let person = {
+//   firstName: 'Pulatov',
+//   job: 'teacher',
+//   age: 31,
+//   friends: ['Elena', 'Igor'],
+// };
+// let car = {
+//   name: 'Ford',
+//   model: 'Focus',
+//   year: 2018,
+// };
+
+// let printPerson = printObject.bind(person);
+// printPerson('Person');
+
+// printObject.call(car, 'Car');
+
+// printObject.apply(person, ['Person']);
+
+//  * Контекст
+
+// let person = {
+//   age: 28,
+//   name: 'Max',
+//   job: 'Frontend',
+
+//   displayInfo: function (ms) {
+//     let self = this;
+//     setTimeout(function () {
+//       console.log('Name:', self.name);
+//       console.log('Job:', self.job);
+//       console.log('Age:', self.age);
+//     }, ms);
+//   },
+// };
+
+// person.displayInfo(500);
+
+//  *Замыкание
+// let createCounter = function (counterName) {
+//   let counter = 0;
+
+//   return {
+//     increment: function () {
+//       counter++;
+//     },
+//     decrement: function () {
+//       counter--;
+//     },
+//     getCounter: function () {
+//       return counter;
+//     },
+//   };
+// };
+
+// let counterA = createCounter('Countert A');
+// let counterB = createCounter('Countert B');
+
+// counterA.increment();
+// counterA.increment();
+// counterA.increment();
+
+// counterB.increment();
+// counterB.increment();
+// counterB.increment();
+
+// * Получаем ключ от обьекта
+// let person = {
+//   name: 'Max',
+//   age: 24,
+//   job: 'Frontend',
+// };
+
+// Object.keys(person).forEach(function(key) {   // первый метод получение ключа
+//   console.log(person[key]);
+// })
+
+// let keys = Object.keys(person); //
+// console.log(keys);
+
+// for (let key in person) {        // второй метод получение ключа
+//   console.log(person[key]);
+// }
+
+// let ford = Object.create(
+//   Object - глобальный обьект; create - метод
+
+//   Первый Параметр - create
+//   {
+//     calculateDistancePerYear: function () {
+//       // высчитываем сколько машина проезжает за год
+//       Object.defineProperty(this, 'distancePerYear', {
+//         //1. this - обьект , 2. название , 3. создаем обьект
+//         value: Math.ceil(this.distance / this.age), // math.ceil - округляет число до целого
+//         enumerable: true,
+//         writable: false,
+//         configurable: false,
+//       });
+//     },
+//   },
+
+//   //Второй Параметр - create
+//   {
+//     name: {
+//       value: 'Ford',
+//       enumerable: true, // позволяет открыть область видимости для опред. свойств и будет дост. для перечесление
+//       writable: false, // св-во может быть изменино если значение true (по умол. знач false), нельзя поменять 'Ford'
+//       configurable: false, // мы можем удалять или изменять ключи из обьекта если знач true (по умол. знач false)
+//     },
+//     model: {
+//       value: 'Focus',
+//       enumerable: true,
+//       writable: false,
+//       configurable: false,
+//     },
+//     year: {
+//       value: 1997,
+//       enumerable: true,
+//       writable: false,
+//       configurable: false,
+//     },
+//     distance: {
+//       value: 144888,
+//       enumerable: true,
+//       writable: true,
+//       configurable: false,
+//     },
+
+//     age: {
+//       // высчитываем возраст машины
+//       enumerable: true,
+//       get: function () {
+//         console.log('Получаем возраст');
+//         return new Date().getFullYear() - this.year; //создаем новый обект(new Date)-вызываем метод(текущий год ) - значение
+//       },
+//       set: function () {
+//         console.log('Устанавливаем значение ');
+//       },
+//     },
+//   }
+// );
+
+// console.log(ford);
+
+// for (let key in ford) {
+//   // получаем ключи нашего обьекта
+//   console.log(key);
+// }
 // function Car(name, year) {
 //   this.name = name;
 //   this.year = year;
